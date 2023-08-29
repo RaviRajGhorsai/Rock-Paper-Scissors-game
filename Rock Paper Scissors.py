17dd8bbe0ec0ba76854ed1c9b1b79b7c5a7a9a62
RaviRajGhorsai <118537291+RaviRajGhorsai@users.noreply.github.com>
2023-08-29 18:38:01 +0545

    A console game

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -12,20 +12,20 @@ while(end != 'n'):
     print(f"Player: {user_action}\ncomputer: {computer_action}.\n")
 
     if user_action == computer_action:
-        print(f"Both player selected {user_action} so it's a tie.\n")
+        print("Tie!!\n")
     elif user_action == "rock":
         if computer_action == "scissors":
-            print("Rock smashes scissors. So you win!!\n")
+            print("You win!!\n")
         else:
-            print("Paper covers rock. So you lose..\n")
+            print("You lose..\n")
     elif user_action == "paper":
         if computer_action == "rock":
-            print("Paper covers rock. So you win!!\n")
+            print("You win!!\n")
         else:
-            print("Scissors cuts paper. So you lose..\n")
+            print("You lose..\n")
     elif user_action == "scissors":
         if computer_action == "paper":
-            print("Scissors cuts paper. So you win!!\n")
+            print("You win!!\n")
         else:
-            print("Rock smashes scissors. So you lose..")
+            print("You lose..")
     end = input("Do you want to play again: Y/N:  ").lower()
